0e2677e9fdd9f98b476bfe9290e321cfee382d63
martypdx <marty@martypdx.com>
2018-03-09 10:41:50 -0800

    script the parts of express app creation

--- 10-functional-programming/demos/node-pg/server.js
+++ 10-functional-programming/demos/node-pg/server.js
@@ -1,1 +1,18 @@
 'use strict';
+
+/* 1) Environment Variables */
+
+
+/* 2) Required Dependecies */
+
+
+/* 3) Create DB client and connect */
+
+
+/* 4) Create express app and setup middleware */
+
+
+/* 5) Data API Routes */
+
+
+/* 6) Start server */
