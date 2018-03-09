acf4906e9c4e47d91140061ef768fd43c3ba084e
martypdx <marty@martypdx.com>
2018-03-09 10:39:14 -0800

    array filter map example and setup for more map, filter, reduce

--- 10-functional-programming/demos/node-pg/array-map.js
+++ 10-functional-programming/demos/node-pg/array-map.js
@@ -10,7 +10,7 @@ const peoples = [
     { name: 'timmy', start: 5, end: 19 },
     { name: 'tommy', start: 7, end: 19 },
     { name: 'tummy', start: 20, end: 29 },
-    { name: 'temmy', start: 7, end: 10 },
+    { name: 'tymmy', start: 7, end: 10 },
 ];
 
 // 1) basic map
@@ -18,18 +18,21 @@ const stats = peoples.map(person => {
     return {
         name: person.name,
         distance: person.end - person.start
-    }
+    };
 });
 
-// 2) Same thing with implicit arrow function return
-// const stats = peoples.map(person => ({
-//     name: person.name,
-//     distance: person.end - person.start
-// }));
+console.log(stats);
 
+// 2) Same thing with implicit arrow function return
+const stats2 = peoples.map(person => ({
+    name: person.name,
+    distance: person.end - person.start
+}));
+
+console.log(stats2);
 
 // 3) Same thing, but map "work" is done in an external function 
-const stats = peoples.map(person => {
+const stats3 = peoples.map(person => {
     return makePersonStats(person);
 });
 
@@ -41,4 +44,4 @@ function makePersonStats(person) {
 }
 
 
-console.log(stats);
+console.log(stats3);
